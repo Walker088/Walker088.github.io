976932ced35733cc7ab600b94ac037218f5bad74
walker088 <walker088@walker088.tw>
2021-09-04 03:00:55 -0400

    feat: switch to hugo-minima

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function () {
+    //const light = '🌝', dark = '🌚';
+    const light = '🐕', dark = '🐈‍⬛';
+    const LIGHT = 'light', DARK = 'dark';
+    const themeSwitcher = document.getElementById('theme-switcher');
+
+    themeSwitcher.innerHTML = localStorage.theme === LIGHT ? light : dark;
+
+    themeSwitcher.addEventListener('click', function () {
+        const currentTheme = localStorage.theme,
+            newTheme = currentTheme === LIGHT ? DARK : LIGHT,
+            { classList } = document.querySelector('html'),
+            text = newTheme === LIGHT ? light : dark;
+        classList.remove(currentTheme);
+        classList.add(newTheme);
+        localStorage.theme = newTheme;
+        themeSwitcher.innerHTML = text;
+    });
+});
